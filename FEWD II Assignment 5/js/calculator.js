3a575414c2331469bae3d777a03ba58de9b75fcf
stefaniahernandez <47236121+stefaniahernandez@users.noreply.github.com>
2019-04-04 21:35:13 -0700

    Add files via upload

--- FEWD II Assignment 5/js/calculator.js
+++ FEWD II Assignment 5/js/calculator.js
@@ -1,2 +1,32 @@
 /*eslint-env browser*/
 
+var $ = function (id) {
+    "use strict";
+    return window.document.getElementById(id);
+};
+
+function init() {
+    "use strict";
+    document.getElementById("calculator").addEventListener("click", init {
+        function enter() {}
+    });
+}
+
+function enter() {
+    "use strict";
+    document.getElementById("result")
+}
+
+function init() {
+    "use strict";
+    document.getElementById("calculator").addEventListener("click", init {
+        enter();
+    });
+}
+
+function calculate() {
+    "use strict";
+    document.getElementById("result").addEventListener("click", calculate {
+                                                       
+                                                       })
+}
